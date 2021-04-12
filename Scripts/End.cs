5e6cbd5e14d4879f06fae596278e1ed7fe4c02ed
epamatz <40709011+epamatz@users.noreply.github.com>
2021-04-11 23:46:43 -0700

    Add files via upload

---
+++ Scripts/End.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Timers;
+
+public class End : MonoBehaviour
+{
+    public static float BaseHealth=3;
+  void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Enemy")
+        {
+            BaseHealth-=1;
+            if(BaseHealth<=0){
+            SceneManager.LoadScene("TowerDefense");
+             }
+    }
+  }
+}
---
+++ Scripts/PlaceTower9001.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower9001 : MonoBehaviour
+{
+  public GameObject Tower;
+
+  public GameObject World;
+    // Start is called before the first frame update
+    void Start()  
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Purse.coins>=40){
+      if (Input.GetMouseButtonDown(0))
+      {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+          if (hit.transform.tag == "TowerSpot")
+          {
+            //Book keeping
+            // if good 
+            hit.transform.gameObject.SetActive(false);
+            Purse.coins-=40;
+          PlaceTower(hit.transform.position);
+          }
+        
+      } 
+    }
+
+    //raycast
+    //hitplace
+    //purse script $$$$
+    //instantiate a tower
+
+  }
+
+    void PlaceTower(Vector3 position)
+    {
+      //Book keeping
+      Instantiate(Tower, position, Quaternion.identity, World.transform);
+    }
+}
